82ad58b91881759f0a9f47d1b7313fb06ff295f5
wangrong <wangrong@saicmotor.com>
2020-03-23 17:41:42 +0800

    add delay api-port 9090

--- gin.go
+++ gin.go
@@ -24,30 +24,45 @@ func GetCurrentTime() string {
 	return timer.String()
 }
 
+var delaySecond int = 65
 
 func main() {
 	var port = "8080" //os.Args[1]
 	fmt.Printf("--------------port: %v\n", port)
 	r := gin.Default()
-/*	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})*/
+
 	r.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
-			"message": "resp host:" + GetHostName() + "\n",
+			"message": "resp host:" + GetHostName() ,
 		})
 	})
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
-			"message": "time:" + GetCurrentTime() + "\n",
+			"message": "time:" + GetCurrentTime(),
 		})
-
-		// Handle all requests using net/http
-		http.Handle("/", r)
 	})
 
 	fmt.Printf("===============port: %v============\n", port)
-	r.Run(":"+port) // listen and serve on 0.0.0.0:8080
+	go r.Run(":" + port) // listen and serve on 0.0.0.0:8080
+
+
+
+	//延迟测试
+	var port1 = "9090"
+
+	mux1 := http.NewServeMux()
+	mux1.HandleFunc("/", myHandler1)
+	fmt.Printf("===============port: %v========Resp delay %vs====\n", port1, delaySecond)
+	go http.ListenAndServe(":9090", mux1)
+
+
+	//阻塞程序
+	select {}
+}
+
+func myHandler1(res http.ResponseWriter, req *http.Request)  {
+
+	time.Sleep(time.Second * time.Duration(delaySecond))
+	//fmt.Println(req.URL, req.Host)
+	fmt.Println("time:" + GetCurrentTime())
 }
